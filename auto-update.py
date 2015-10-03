6a865c786882eac9bc7b9d1b2fcbc51c18d1ddd3
itstehkman <itstehkman@gmail.com>
2015-10-03 01:31:17 -0700

    Allow server to listen to pushes and update itself

--- auto-update.py
+++ auto-update.py
@@ -1,14 +1,13 @@
 from flask import Flask
 import subprocess
-import requests
 
 app = Flask(__name__)
 
-@app.route('/payload', methods = ['POST'])
+@app.route('/payload', methods = ['GET', 'POST'])
 def update():
 	subprocess.Popen(['git', 'pull'])
+	return 'success!'
 
 if __name__ == '__main__':
-	app.run()
-	requests.post('http://104.236.191.45', data = {})
+	app.run(host='0.0.0.0')
 
